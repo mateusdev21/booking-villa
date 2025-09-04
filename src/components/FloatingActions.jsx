9fdc7f71716df6cf3d4c29dbe2a69b9e3b4a24d2
mateusdev21 <mateus.dev21@gmail.com>
2025-09-04 11:42:30 +0700

    adding dialog box

--- src/components/FloatingActions.jsx
+++ src/components/FloatingActions.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaCaretDown } from "react-icons/fa";
 import { LuCalendarDays } from "react-icons/lu";
 
 const FloatingActions = () => {
+  const [open, setOpen] = useState(false);
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dialogRef.current && !dialogRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open]);
+
   return (
     <div className="flex relative justify-center">
       <div className="flex bg-white absolute -top-14 py-8 px-20 rounded-md shadow-sm/20">
@@ -19,11 +36,39 @@ const FloatingActions = () => {
           <LuCalendarDays className="ml-2 text-[#106A64]" />
         </div>
         <div className="flex flex-[1] items-center pr-8 pl-4 text-nowrap">
-          <button className="py-2 px-8 bg-[#106A64] text-white rounded-md hover:opacity-80 hover:cursor-pointer">
+          <button
+            onClick={() => setOpen(true)}
+            className="py-2 px-8 bg-[#106A64] text-white rounded-md hover:opacity-80 hover:cursor-pointer"
+          >
             Check avability
           </button>
         </div>
       </div>
+      {open && (
+        <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+          {/* Dialog box */}
+          <div
+            ref={dialogRef}
+            className="bg-white rounded-lg shadow-lg w-80 p-6 relative"
+          >
+            <h2 className="text-2xl font-semibold text-black text-center">
+              Available
+            </h2>
+            <p className="mt-2 text-[#777777] text-center text-xs">
+              ROOM on START_DATE until END_DATE is available
+            </p>
+
+            <div className="mt-4">
+              <button
+                onClick={() => setOpen(false)}
+                className="px-4 py-2 min-w-[100%] rounded-md bg-[#106A64] text-white text-sm hover:opacity-80 hover:cursor-pointer"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
